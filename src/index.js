9c64a519e784286e0756475f65d1bc634e876257
atharvakadam <atharvakadam@gmail.com>
2020-05-30 20:46:26 -0700

    Part 1 commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,78 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+// import App from './App';
+// import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+const Hello = (props) => {
+
+  const bornYear = () => {
+    const yearNow = new Date().getFullYear()
+    return yearNow - props.age
+  }
+
+  return (
+    <div>
+      <p>Hello {props.name}, you are {props.age} years old</p>
+      <p>So you were probably born in {bornYear()}</p>
+    </div>
+  );
+}
+
+const Footer = () => {
+  return (
+    <div>
+      greeting app created by <a href="https://code.atharvakadam.com">Atharva Kadam</a>
+    </div>
+  )
+}
+
+const handleClick = () => {
+  console.log("CLICKED");
+}
+
+const Display = (props) => {
+  return (
+    <div>{props.counter}</div>
+  );
+}
+
+const Button = (props) => {
+  return (
+    <button onClick={props.handleClick}>
+      {props.text}
+    </button>
+  );
+}
+
+
+const App = () => {
+  const answer = useState(0);
+  console.log(answer);
+  const [counter, setCounter] = answer
+  const increaseByOne = () => setCounter(counter + 1);
+  const decreaseByOne = () => setCounter(counter - 1);
+  const setToZero = () => setCounter(0);
+  // setTimeout(
+  //   () => setCounter(counter + 1),
+  //   1000
+  // )
+
+  return (
+  <div>
+  <Display counter={counter} />
+  <Button text='plus' handleClick={increaseByOne} />
+  <Button text='zero' handleClick={setToZero} />
+  <Button text='minus' handleClick={decreaseByOne} />
+  </div>
+  
+  );
+}
+
+
+ReactDOM.render( <App />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+// serviceWorker.unregister();
